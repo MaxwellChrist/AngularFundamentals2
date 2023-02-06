c2c4ede81754be0fb3a70863a460121292a5a495
MaxwellChrist <ribthulu@gmail.com>
2023-02-06 17:24:00 -0500

    practicing data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit{
+  allowNewServer = false;
+  serverCreationStatus = "No server was created";
+  serverName = '';
+  serverType = '';
+
+  username = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
+
+  ngOnInit() {
+
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName + " and the type is " + this.serverType;
+  }
+
+  onUpdateServerName(input: Event) {
+    this.serverName = (<HTMLInputElement>input.target).value
+  }
+
+  onUpdateUsername() {
+    return this.username = ''
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-import { SuccessAlertComponent } from './success/success.component'
-import { WarningAlertComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent
+    ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
