b9dc622d8b8c98c78927a696dd536f09fd746aec
MaxwellChrist <ribthulu@gmail.com>
2023-02-08 12:44:37 -0500

    tweaked some styling of recipe list component

--- src/app/feature-recipe/recipes/recipe-list/recipe-list.component.ts
+++ src/app/feature-recipe/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,6 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', "this is a test", "https://static01.nyt.com/images/2022/03/02/dining/kc-pasta-amatriciana/merlin_201502869_17a64c2f-adee-4526-9951-a0b03270b022-articleLarge.jpg?quality=75&auto=webp&disable=upscale")
+    new Recipe('Test Recipe', "this is a test", "https://static01.nyt.com/images/2022/03/02/dining/kc-pasta-amatriciana/merlin_201502869_17a64c2f-adee-4526-9951-a0b03270b022-articleLarge.jpg?quality=75&auto=webp&disable=upscale"),
   ];
 }
