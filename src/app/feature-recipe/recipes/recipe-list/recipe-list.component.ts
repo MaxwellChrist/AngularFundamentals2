03d99801aeb2f2b6036e1ffacbd433434b0c11b9
MaxwellChrist <ribthulu@gmail.com>
2023-02-08 12:18:54 -0500

    created Recipes class to pass data through recipe-list

--- src/app/feature-recipe/recipes/recipe-list/recipe-list.component.ts
+++ src/app/feature-recipe/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', "this is a test", "https://static01.nyt.com/images/2022/03/02/dining/kc-pasta-amatriciana/merlin_201502869_17a64c2f-adee-4526-9951-a0b03270b022-articleLarge.jpg?quality=75&auto=webp&disable=upscale")
+  ];
 }
